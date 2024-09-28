8bdfd9829435bba65e423ff40dd5319b58c059b8
Dnyaneshwar Argade <dnargade95@gmail.com>
2024-09-28 20:11:35 +0530

    Add files via upload

---
+++ calculator1.py
@@ -0,0 +1,11 @@
+
+#--------------- calculator -------------------------------
+a = 1
+b = 5
+multiplication = (a*b)
+addition = (a+b)
+division = (a/b)
+substraction=(a-b)
+squaew = (a**b)
+floordivision = (a//b)
+print (multiplication,addition,division,substraction,squaew,floordivision,sep=",")
