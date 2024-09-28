b73f2275fbf514d88d89cb5192fab47ce37d5dbc
Dnyaneshwar Argade <dnargade95@gmail.com>
2024-09-28 22:20:26 +0530

    Update calculator1.py

--- calculator1.py
+++ calculator1.py
@@ -9,3 +9,10 @@ substraction=(a-b)
 squaew = (a**b)
 floordivision = (a//b)
 print (multiplication,addition,division,substraction,squaew,floordivision,sep=",")
+
+-----------------------------------------------------------------------------------
+# calculation using input()
+-----------------------------------------------------------------------------------
+a = input("type first number:")
+b = input("type secount number:")
+print(int(a)+int(b))
