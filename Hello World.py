b34890bbd15e0d3d4cc5f6ecf6fd89ca6f0ae824
Dnyaneshwar Argade <dnargade95@gmail.com>
2025-05-13 00:02:29 +0530

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,16 @@
+#Print("Hello World")
+# NameError: name 'Print' is not defined. Did you mean: 'print'?
+#-----------------------------------------------------------------------------------
+#  print("Hello World")
+#  Hello World
+#
+#  print("My name is: Dnyaneshwar")
+#  My name is: Dnyaneshwar
+#
+#  print("At one day fefinately i will get 1cr package")
+#  At one day fefinately i will get 1cr package
+#
+#  print("congratulation dnyaneshwar for getting 1cr package")
+#  congratulation dnyaneshwar for getting 1cr package
+
+print("I will definately donate as much muny as i can if received 1cr")
---
+++ Variable.py
@@ -0,0 +1,117 @@
+"""
+a = 10
+print(a)
+
+b = "Dnyaneshwar"
+print(b)
+
+b = 50000000
+print(b)
+
+a="Dnyaneeshwar"
+print("My name is:",a)
+
+
+1 = "dnyaneesh"
+print(1)
+
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+
+
+1a = "dnyaneesh"
+print(1a)
+SyntaxError: invalid decimal literal
+
+name = "Dnyaneshwar"
+age = 29
+print ("My name is:",name, "and age is -",age)
+My name is: Dnyaneshwar and age is - 29
+
+
+$a = 10
+print($a)
+SyntaxError: invalid syntax
+
+name,age,gender="Dnyanesh",29,"Male"
+print(name,age,gender)
+Dnyanesh 29 Male
+
+
+a,b,c =1,2
+print(a,b,c)
+ValueError: not enough values to unpack (expected 3, got 2)
+
+a=1,2,3
+print(a)
+(1, 2, 3)
+
+a = 1,"Dnyanesh"
+print(a)
+(1, 'Dnyanesh')
+
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+print(a,b)
+print(b,c)
+print(a,c)
+print(a,b,c)
+
+1
+2
+3
+1 2
+2 3
+1 3
+1 2 3
+
+a=1
+print(a)
+1
+
+a=1
+a=2
+print(a)
+2
+
+
+"""
+
+a=1
+a=2
+a=3
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
