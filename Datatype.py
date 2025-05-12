34d6297891e52139369a9cceb159f60f5aceec0d
Dnyaneshwar Argade <dnargade95@gmail.com>
2025-05-13 00:04:13 +0530

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,114 @@
+"""
+-----------------Data Type Practicee in python----------------------
+
+a = 10
+print(a)
+print(type(a))
+10
+<class 'int'>
+
+b = 20
+print(b)
+print(type(b))
+20
+<class 'int'>
+
+c = 20.25
+print(c)
+print(type(c))
+20.25
+<class 'float'>
+
+d = "Dnyanesh"
+primt(d)
+print(type(d))
+NameError: name 'primt' is not defined. Did you mean: 'print'?
+
+d = "Dnyanesh"
+print(d)
+print(type(d))
+Dnyanesh
+<class 'str'>
+
+
+e =true
+print(e)
+print(type(e))
+NameError: name 'true' is not defined. Did you mean: 'True'?
+
+e =True
+print(e)
+print(type(e))
+True
+<class 'bool'>
+
+a =None
+print(a)
+print(type(a))
+None
+<class 'NoneType'>
+
+list=(1,2,33,4,5)
+print(list)
+print(type(list))
+(1, 2, 33, 4, 5)
+<class 'tuple'>
+
+a =[1,2,3,4,5,6,7,8,9,0]
+print(a)
+print(type(a))
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+<class 'list'>
+
+
+a = {1,2,3,4,5,6,7,8,9}
+print(a)
+print(type(a))
+{1, 2, 3, 4, 5, 6, 7, 8, 9}
+<class 'set'>
+
+a = {1:"apple",2:"orange",3:"Avacado"}
+print(a)
+print(type(a))
+{1: 'apple', 2: 'orange', 3: 'Avacado'}
+<class 'dict'>
+
+a=("dnyanesh",1)
+print(a)
+print(type(a))
+('dnyanesh', 1)
+<class 'tuple'>
+
+a=["dnyanesh",1]
+print(a)
+print(type(a))
+['dnyanesh', 1]
+<class 'list'>
+
+a={"dnyanesh",1}
+print(a)
+print(type(a))
+{'dnyanesh', 1}
+<class 'set'>
+
+
+a={1:"dnyanesh",2:2}
+print(a)
+print(type(a))
+{1: 'dnyanesh', 2: 2}
+<class 'dict'>
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
