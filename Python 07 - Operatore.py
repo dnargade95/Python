69a2bc76a9f66a4f782c567f5410dc6eeaeb96ed
Dnyaneshwar Argade <dnargade95@gmail.com>
2025-05-20 22:54:09 +0530

    Add files via upload

---
+++ Python 07 - Operatore.py
@@ -0,0 +1,101 @@
+"""
+# Operatores in python - Arithmatic 
+
+a = 10
+b = 20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+30
+-10
+200
+0.5
+
+a = 10
+b = 20
+print(a**b)
+100000000000000000000
+
+a = 10
+b = 20
+print(a%b)
+10
+
+
+# Relational Operator 
+
+a = 10
+b = 20
+print(a>b)
+False
+
+a = 10
+b = 20
+print(a<b)
+
+True
+
+a = 10
+b = 20
+print(a<=b)
+
+True
+
+a = 10
+b = 20
+print(a>=b)
+False
+
+a = 20
+b = 20
+print(a == b)
+
+a = 20
+b = 20
+print(a != b)
+False
+
+
+a = 10
+b = 20
+c = 30
+
+print(a>b and b>c)
+
+False
+
+a = 10
+b = 20
+c = 30
+
+print(a<b and b>c)
+False
+
+a = 10
+b = 20
+c = 30
+
+print(a<b or b>c)
+True
+
+
+a =[10,20,30,40]
+print( 10 in a)
+
+True
+
+a =[10,20,30,40]
+print( 100 in a)
+False
+
+a =[10,20,30,40]
+print( 100 not in a)
+True
+
+"""
+
+a =[10,20,30,40]
+print( 100 not in a)
+True
