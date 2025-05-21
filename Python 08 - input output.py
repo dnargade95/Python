ad39425e8143ed1f0f331017f80481693fb547c9
Dnyaneshwar Argade <dnargade95@gmail.com>
2025-05-21 09:44:13 +0530

    Update Python 08 - input output.py

--- Python 08 - input output.py
+++ Python 08 - input output.py
@@ -1,1 +1,22 @@
  --------Input output------- 
+
+name = input("Inter your name here:")
+age = input("What is your age?")
+print("Hi ",name,", Good Morning","Your are",age,"year old" )
+
+Inter your name here:Dnyaneshwar
+What is your age?28
+Hi  Dnyaneshwar , Good Morning Your are 28 year old
+
+a = input("Inter First value : ")
+b = input("Inter Secound value : ")
+
+print("Addition" , int(a) + int(b))
+print("Substraction" , int(a) - int(b))
+print("Multiplication" , int(a) * int(b))
+
+Inter First value : 100
+Inter Secound value : 200
+Addition 300
+Substraction -100
+Multiplication 20000
